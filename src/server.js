0b30b65adf12b29f7b526fac552955339605205d
seul <psk6425@naver.com>
2023-07-25 15:43:19 +0900

    4commit

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.set("view engin", "pug");
 app.set("views",__dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home.pug"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http:localhost:3000`);
 app.listen(3000, handleListen);
