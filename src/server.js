758aff1c1104a2bc1d8d967fb47743e4ad159938
seul <psk6425@naver.com>
2023-07-26 13:24:00 +0900

    7월26일

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,6 +11,14 @@ app.get("/", (req, res) => res.render("home.pug"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http:localhost:3000`);
-app.listen(3000, handleListen);
 
+const server =  http.createServer(app);
 
+const wss = new WebSocket.Server({ server });
+
+function handleConnection (socket) {
+    console.log(socket);
+}
+wss.on("connection",handleConnection);
+
+server.listen(3000, handleListen);
