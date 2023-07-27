afb85597d4ab76b2b709ab40c4ab53f9bce6c48f
seul <psk6425@naver.com>
2023-07-27 10:55:49 +0900

    7월27일

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,11 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
+function handleOpen ()  {
     console.log("Connected to Server✅");
-});
+}
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
     console.log("New message: " , message.data);
@@ -12,6 +15,14 @@ socket.addEventListener("close", () => {
     console.log("Disconnected from Server❌");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+function handleSubmit (event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit); 
+
+
+//frontend
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from "express";
 
 const app = express();
 
-app.set("view engin", "pug");
+app.set("view engine", "pug");
 app.set("views",__dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home.pug"));
@@ -14,14 +14,20 @@ const handleListen = () => console.log(`Listening on http:localhost:3000`);
 
 const server =  http.createServer(app);
 const wss = new WebSocket.Server({ server });
+function onSocketClose() {
+    console.log("Disconnected from the Browser❌");
+}
+
+const sockets = [];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser✅");
-    socket.on("close", () => console.log("Disconnected from the Browser❌"));
-    socket.on("message", (message) => {
-        console.log(message.toString('utf-8'));
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => { 
+        sockets.forEach((aSocket) => aSocket.send(message.toString('utf-8')));
     });
-    socket.send("hello~!");
 });
 
 server.listen(3000, handleListen);
+//backend 부분
